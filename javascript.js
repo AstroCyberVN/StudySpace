001b0da502beb370aedf05f75a224ef2e9ae4ca5
AstroCyberVN <astrocyberr@gmail.com>
2026-08-19 16:50:00 +0700

    [ Test ] Second commit

--- javascript.js
+++ javascript.js
@@ -1,5 +1,13 @@
 // import { CONFIG } from "./config";
 
+function tabSelect() {
+    const currentPath = window.location.pathname;
+
+    document.querySelectorAll('.sidebar ul li a').forEach(link => {
+        link.classList.toggle('selected', link.pathname == currentPath)
+    })
+}
+
 async function loadComponent(url, placeholderId) {
     const result = await fetch(url);
     const html = await result.text();
@@ -8,6 +16,9 @@ async function loadComponent(url, placeholderId) {
 
 async function start() {
     await loadComponent('./shared/navbar.html', 'navbar');
+    await loadComponent('./shared/sidebar.html', 'sidebar');
+
+    tabSelect();
 }
 
 start();
